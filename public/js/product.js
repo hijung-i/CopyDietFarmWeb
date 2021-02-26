0063ff2ef8f1a5a5c503a9fb0bf17e18737f174e
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-02-26 18:25:13 +0900

    상품상세 작업 진행중

--- public/js/product.js
+++ public/js/product.js
@@ -9,8 +9,23 @@ function getProductDetail(){
         productCode: productCode
     }
 
-    ajaxCallWithLogin(API_SERVER + '/product/productDetail', params, 'POST'
+    ajaxCallWithLogin(API_SERVER + '/product/getProductDetail', params, 'POST'
     , function (data) {
+        var product = data.result;
+        if(product == undefined || product.length == 0){
+            // TODO: Open alert modal
+            return false;
+        }
+        console.log(product);
+        $('.detail_title h2').html(product.productName);    
+        $('.product_wrap .product h2').html(product.productName);
+        $('.infoArea01 .product_name_css .con span').html(product.productName);
+
+        $('.infoArea01 .simple_desc_css .con span span').html(product.productDesc);
+        $('.infoArea01 .product_custom .con span span').html(numberFormat(product.discountPrice)+'원');
+        $('.infoArea01 .product_price .con strike').html(numberFormat(product.supplyPrice)+'원');
+        $('.infoArea01 .discount_rate .con strong').html(numberFormat(product.discountRate)+'%');
+
         console.log("productDetail success", data);
     }, function (err) {
         console.log("productDetail error", err);
