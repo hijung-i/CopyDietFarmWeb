13c08df5457aee05738f21a63cc484ac749b67e0
hKim95S2 <gmhmhmhg@gmail.com>
2021-03-24 16:05:58 +0900

    0324 머지

--- src/routes/indexViewRouter.ts
+++ src/routes/indexViewRouter.ts
@@ -1,17 +1,13 @@
 import { NextFunction, request, Request, Response, Router } from 'express'
-
 const router = Router()
-
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     const isLoggedIn: boolean | undefined = req.session.isLoggedIn
     if (isLoggedIn === true) {
         console.log('session', req.session)
         console.log('sessionId', req.sessionID)
     }
-
     render(res, 'index', {})
 })
-
 router.get('/login-form', (req: Request, res: Response, next: NextFunction) => {
     const isLoggedIn: boolean | undefined = req.session.isLoggedIn
     console.log(isLoggedIn)
@@ -19,10 +15,8 @@ router.get('/login-form', (req: Request, res: Response, next: NextFunction) => {
         res.redirect('/')
         return
     }
-
     render(res, 'login', {})
 })
-
 router.get('/sign-up-form', (req: Request, res: Response, next: NextFunction) => {
     const isLoggedIn: boolean | undefined = req.session.isLoggedIn
     console.log(isLoggedIn)
@@ -30,15 +24,12 @@ router.get('/sign-up-form', (req: Request, res: Response, next: NextFunction) =>
         res.redirect('/')
         return
     }
-
     render(res, 'sign_up', {})
 })
-
 router.get('/product/:productCode', (req: Request, res: Response, next: NextFunction) => {
     const productCode = req.params.productCode
     render(res, 'product', { productCode: productCode })
 })
-
 router.get('/products/:salesStandCode/event', (req: Request, res: Response, next: NextFunction) => {
     const standCode = req.params.salesStandCode
     console.log(standCode)
@@ -48,7 +39,6 @@ router.get('/products/:salesStandCode/event', (req: Request, res: Response, next
         listType: 'EVENT'
     })
 })
-
 router.get('/products/:category1Code/category/:category2Code', (req: Request, res: Response, next: NextFunction) => {
     const category1Code = req.params.category1Code
     const category2Code = req.params.category2Code
@@ -59,47 +49,54 @@ router.get('/products/:category1Code/category/:category2Code', (req: Request, re
         listType: 'CATEGORY'
     })
 })
-
 router.get('/search-form', (req: Request, res: Response, next: NextFunction) => {
     render(res, 'search', {})
 })
-
 router.get('/mypage', (req: Request, res: Response, next: NextFunction) => {
     const isLoggedIn = req.session.isLoggedIn || false
     console.log('isLoggedIn ->> ', isLoggedIn)
     render(res, 'my_page', { isLoggedIn: isLoggedIn })
 })
-
 router.get('/faq', (req: Request, res: Response, next: NextFunction) => {
     render(res, 'faq', {})
 })
-
 router.get('/notice', (req: Request, res: Response, next: NextFunction) => {
     render(res, 'notice', {})
 })
-
 router.get('/notice/:boardNo', (req: Request, res: Response, next: NextFunction) => {
     const boardNo = req.params.boardNo
     render(res, 'notice_detail', { boardNo: boardNo })
 })
-
 router.get('/cs-center', (req: Request, res: Response, next: NextFunction) => {
     render(res, 'cs_center', {})
 })
-
 router.get('/privacyPolicy', (req: Request, res: Response, next: NextFunction) => {
     render(res, 'PrivacyPolicy', {})
 })
-
 router.get('/invite', (req: Request, res: Response, next: NextFunction) => {
     render(res, 'invite', {})
 })
-
 router.get('/delivery-info', (req: Request, res: Response, next: NextFunction) => {
     render(res, 'delivery_info', {})
 })
-
-
+router.get('/pick-product', (req: Request, res: Response, next: NextFunction) => {
+    render(res, 'pick_product', {})
+})
+router.get('/pwdReset', (req: Request, res: Response, next: NextFunction) => {
+    render(res, 'pwdReset', {})
+})
+router.get('/order-comp', (req: Request, res: Response, next: NextFunction) => {
+    render(res, 'order_complete', {})
+})
+router.get('/order-info', (req: Request, res: Response, next: NextFunction) => {
+    render(res, 'order_info', {})
+})
+router.get('/alarm', (req: Request, res: Response, next: NextFunction) => {
+    render(res, 'alarm', {})
+})
+router.get('/terms', (req: Request, res: Response, next: NextFunction) => {
+    render(res, 'TermsOfService', {})
+})
 const render = (res: Response, view: any, data: any | null) => {
     res.render(view, data || null)
 }
