83b411cd0a161debc6e70d2732cf951cef9e283e
hijung-i <hijung@data-flow.co.kr>
2021-03-25 09:38:26 +0900

    ...

--- src/routes/indexViewRouter.ts
+++ src/routes/indexViewRouter.ts
@@ -26,9 +26,9 @@ router.get('/sign-up-form', (req: Request, res: Response, next: NextFunction) =>
     }
     render(res, 'sign_up', {})
 })
-router.get('/mypage-orderlistall', (req: Request, res: Response, next: NextFunction) => {
+router.get('/mypage-orderlist', (req: Request, res: Response, next: NextFunction) => {
     const productCode = req.params.productCode
-    render(res, 'mypage_orderListAll', { productCode: productCode })
+    render(res, 'mypage_orderList', { productCode: productCode })
 })
 router.get('/product/:productCode', (req: Request, res: Response, next: NextFunction) => {
     const productCode = req.params.productCode
