3b2d82e77bfdd4a21ba3425f48e4b1374c5b6bba
hKim95S2 <gmhmhmhg@gmail.com>
2021-03-30 11:50:42 +0900

    웹 모바일 합치는 중 코드 다시씀

--- public/js/header.js
+++ public/js/header.js
@@ -79,3 +79,46 @@ $(function() {
         });
     });
 });
+//웹 햄버거 열기 및 닫기
+$(document).ready(function() {
+
+    $('.btnMenu>a').on('click', function() {
+        $('.gnb').hide();
+    });
+});
+$('.slideMenu_close>a').on('click', function() {
+    $('.gnb').show();
+});
+
+$(document).ready(function(){
+$("#gnbAllMenu").hide();
+$("#btnGnbOpen").click(function(){
+    $("#gnbAllMenu").slideToggle("fast");
+});
+});
+//웹 햄버거 depth02
+$(document).ready(function() {
+    $(".m_slides_sub").hide();
+    $(".m_ba_slides_n_w li").hover(function(){
+        $("ul:not(:animated)",this).slideDown("700");
+
+        $(".m_ba_slides_n_w li a").removeClass("active");
+    },
+    function() {
+        $("ul",this).slideUp("700");
+       });
+
+    $("#memberMenu").bind("moseover mouseenter",function(){
+        $("#memMenu").show();
+  });
+   $("#memMenu").bind("moseout mouseleave",function(){
+        $("#memMenu").hide();
+  });
+
+    $("#NotmemberMenu").bind("moseover mouseenter",function(){
+        $("#NotmemMenu").show();
+  });
+   $("#NotmemMenu").bind("moseout mouseleave",function(){
+        $("#NotmemMenu").hide();
+  });
+});
