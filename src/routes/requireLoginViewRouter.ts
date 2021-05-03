86e5ee86efe6222f79d7199c8e0cf76e353a89fa
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-05-03 14:29:50 +0900

    주문하기 데이터 연결 진행중

--- src/routes/requireLoginViewRouter.ts
+++ src/routes/requireLoginViewRouter.ts
@@ -23,17 +23,26 @@ router.get('/cart/:userId', (req: Request, res: Response, next: NextFunction) =>
 
 router.get('/order', (req: Request, res: Response, next: NextFunction) => {
     const deliveryGroupList = req.query.deliveryGroupList
-    console.log(deliveryGroupList)
-    const orderDTO = req.query.orderDTO
+    const orderDTO = JSON.parse(req.query.orderDTO as string)
+
+    orderDTO.userId = 'jgpark'
+    orderDTO.userName = '박진국'
+    orderDTO.email = 'jgpark@data-flow.co.kr'
+    orderDTO.userCellNo = '01055431787'
+    orderDTO.address = '01129, 서울시 강북구 덕릉로 30길 9-20(미아동)2층 ( 계단 한 번)'
 
     // checkLogin(req, res, (sessionUser: SessionUser) => {
     //     if (userId !== sessionUser.userId) {
-    //         res.send('<script>alert("잘못된 접근입니다.");location.href = "/";</script>')
+        //         res.send('<script>alert("잘못된 접근입니다.");location.href = "/";</script>')
     //     } else {
+            // session data input
+            // orderDTO.userId = sessionUser.userId
+            // orderDTO.userName = sessionUser.userName
+            // orderDTO.email = sessionUser.email
     //         render(res, 'cart', { userId: userId })
     //     }
     // })
-    render(res, 'order_info', { deliveryGroupList, orderDTO })
+    render(res, 'order_info', { deliveryGroupList, orderDTO: JSON.stringify(orderDTO) })
 })
 
 router.get('/delivery-management', (req: Request, res: Response, next: NextFunction) => {
