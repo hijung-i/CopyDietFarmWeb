48860adfa3f8e9ceb2bc4a99cd7f2fb8bb81020f
hKim95S2 <hlkim@data-flow.co.kr>
2021-05-24 14:04:06 +0900

    이벤트리스너 오류

--- public/js/common.js
+++ public/js/common.js
@@ -362,15 +362,10 @@ function setCookie( name, value, expiredays ) {  // 쿠키저장
   
 
 $(function(){
-	$(".popup_box").draggable({containment:'parent', scroll:false}); // 레이어 팝업 창 드래그 가능
+	//$(".popup_box").draggable({containment:'parent', scroll:false}); // 레이어 팝업 창 드래그 가능
 	//{containment:'parent', scroll:false} 화면 영역 밖으로 드래그 안됌.
 				
-	if(document.cookie.indexOf("popToday=close") < 0 ){      // 쿠키 저장여부 체크
-		document.getElementById("popup_layer").style.display = "block";
-	}else {
-		document.getElementById("popup_layer").style.display = "none"; 
-	}
-	
+
 	var modal = document.getElementById('myModal');
 	var btn = document.getElementById('myBtn');
 	var span = document.getElementsByClassName('close')[0];
