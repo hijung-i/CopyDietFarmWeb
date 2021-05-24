919603245bafd7f36e6928e834157cb3ad377289
hKim95S2 <hlkim@data-flow.co.kr>
2021-05-24 14:29:34 +0900

    ㅇㅇ

--- public/js/common.js
+++ public/js/common.js
@@ -365,30 +365,30 @@ $(function(){
 	//$(".popup_box").draggable({containment:'parent', scroll:false}); // 레이어 팝업 창 드래그 가능
 	//{containment:'parent', scroll:false} 화면 영역 밖으로 드래그 안됌.
 				
-
 	var modal = document.getElementById('myModal');
-	var btn = document.getElementById('myBtn');
-	var span = document.getElementsByClassName('close')[0];
-	if(btn != null) {
-		btn.addEventListener('click', showModal());
-	}
-	if( span != null) {
-		span.addEventListener('click', hideModal());
+ 
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];                                          
+
+	// When the user clicks on the button, open the modal 
+	btn.onclick = function() {
+		modal.style.display = "block";
 	}
 
-	function showModal() {
-		modal.style.display = 'block';
-	};
-
-	function hideModal() {
-		modal.style.display = 'none';
-	};
-
-	window.addEventListener('click', function(event) {
-	if (event.target != modal) { // needs to be anything but the modal, from what i can understand
-		hideModal();
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
 	}
-	});
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
 });
 
 	
