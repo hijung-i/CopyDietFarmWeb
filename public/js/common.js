0fe97a96bf2026020abcb66574dc9aa887649687
hKim95S2 <hlkim@data-flow.co.kr>
2021-05-28 16:25:07 +0900

    배포용 커밋

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,8 @@
 // var API_SERVER = "http://localhost:9090";
 // var SERVER_IP = 'localhost';
-// var SERVER_IP = 'data-flow.co.kr';
+var SERVER_IP = 'data-flow.co.kr';
 
-// var CALLBACK_SERVER = "http://"+ SERVER_IP +":3000";
+var CALLBACK_SERVER = "http://"+ SERVER_IP +":3000";
 // var API_SERVER = "http://"+ SERVER_IP +":9090";
 
 // var API_SERVER = "http://112.217.209.162:9090";
