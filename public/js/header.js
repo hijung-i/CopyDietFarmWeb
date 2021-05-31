d25f93e91261fee5ed748e58cb771d9222b205a2
hijung-i <hijung@data-flow.co.kr>
2021-05-31 23:23:12 +0900

    push

--- public/js/header.js
+++ public/js/header.js
@@ -306,44 +306,22 @@ $(function(){
 });
 
 /* 롤링 배너 */
-   window.onload = function() {
-            var bannerLeft=0;
-            var first=1;
-            var last;
-            var imgCnt=0;
-            var $img = $(".main_banner img");
-            var $first;
-            var $last;
+window.onload = function() {
+    var bannerLeft=0;
+    var first=1;
+    var last;
+    var imgCnt=0;
+    var $img = $(".main_banner img");
+    var $first;
+    var $last;
 
-            $img.each(function(){   // 1px 간격으로 배너 처음 위치 시킴
-                $(this).css("left",bannerLeft);
-                bannerLeft += $(this).width()+0;
-                $(this).attr("id", "banner"+(++imgCnt));  // img에 id 속성 추가
-            });
+    $img.each(function(){   // 1px 간격으로 배너 처음 위치 시킴
+        $(this).css("left",bannerLeft);
+        bannerLeft += $(this).width()+0;
+        $(this).attr("id", "banner"+(++imgCnt));  // img에 id 속성 추가
+    });
+    if( imgCnt > 1){ //배너 9개 이상이면 이동시킴
 
-            
-            if( imgCnt > 1){                //배너 9개 이상이면 이동시킴
-
-
-
-                last = imgCnt;
-
-                setInterval(function() {
-                    $img.each(function(){
-                        $(this).css("left", $(this).position().left-2); // 2px씩 왼쪽으로 이동
-                    });
-                    $first = $("#banner"+first);
-                    $last = $("#banner"+last);
-                    if($first.position().left < -360) {    // 제일 앞에 배너 제일 뒤로 옮김
-                        $first.css("left", $last.position().left + $last.width()+5 );
-                        first++;
-                        last++;
-                        if(last > imgCnt) { last=1; }   
-                        if(first > imgCnt) { first=1; }
-                    }
-                }, 40);        
-
-    if( imgCnt > 1){                //배너 9개 이상이면 이동시킴
         last = imgCnt;
 
         setInterval(function() {
@@ -359,10 +337,10 @@ $(function(){
                 if(last > imgCnt) { last=1; }   
                 if(first > imgCnt) { first=1; }
             }
-        }, 40);   //여기 값을 조정하면 속도를 조정할 수 있다.(위에 1px 이동하는 부분도 조정하면 
-//깔끔하게 변경가능하다
-    }
-};
+        }, 40); 
+    }    
+
+}
 
 function getCategory() {
     var param = {};
@@ -449,6 +427,4 @@ function getCategory() {
     })
 
 }
-}
-
 
