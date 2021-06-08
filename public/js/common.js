1e21402349384d169174babf63a33c4b172a38b1
hijung-i <hijung@data-flow.co.kr>
2021-06-08 15:02:54 +0900

    pull hlkim

--- public/js/common.js
+++ public/js/common.js
@@ -5,11 +5,11 @@ var SERVER_IP = '192.168.0.3';
 
 // var CALLBACK_SERVER = "http://"+ SERVER_IP +":3000";
 var CALLBACK_SERVER = "http://"+ SERVER_IP;
-var API_SERVER = "http://"+ SERVER_IP +":9090";
+// var API_SERVER = "http://"+ SERVER_IP +":9090";
 
-// var API_SERVER = "http://112.217.209.162:9090";
-// var RESOURCE_SERVER = "http://112.217.209.162:8000";
-// var API_SERVER = "http://13.209.123.102:9090";
+var API_SERVER = "http://112.217.209.162:9090"; // 테스트
+// var RESOURCE_SERVER = "http://112.217.209.162:8000"; // 운영
+// var API_SERVER = "http://13.209.123.102:9090"; // 카카오 
 
 var RESOURCE_SERVER = "http://13.209.123.102:8000";
 
