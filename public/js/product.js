253220a4fcf7f8355703154ef0400d7bc48c1029
hijung-i <hijung@data-flow.co.kr>
2021-06-11 16:33:26 +0900

    product.ejs slick 적용

--- public/js/product.js
+++ public/js/product.js
@@ -53,45 +53,7 @@ $(function() {
         console.log("error", err);
     })
 
-    $('.responsive').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed:3000,
-        pauseOnHover:true,
-        arrow:true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 360,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-    });
+    
 })
 
 function getProductDetail(){
@@ -422,12 +384,69 @@ function getRecommendedProducts() {
         }
 
         app.recmdList = result;
+        insertRecommandListHtml();
     }, function(err) {
         console.error(err);
     }, {
         isRequired: false,
         userId: true
     })
+}
+
+function insertRecommandListHtml() {
+    
+    var recmdList = app.recmdList;
+    var html = '';
+    for(var i = 0; i < recmdList.length; i++) {
+        if(i > 9) {
+            break;
+        }
+        var product = recmdList[i];
+        html += '<div><a href=""><img src="'+ RESOURCE_SERVER + product.url +'" alt="상품 이미지'+ (i+1)+'"></a></div>'
+    }
+
+    console.log(html);
+    $('.responsive').html(html);
+
+    $('.responsive').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:3000,
+        pauseOnHover:true,
+        arrow:true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 360,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
 }
 
 function getProductList() {
@@ -437,14 +456,9 @@ function getProductList() {
     ajaxCallWithLogin(API_SERVER + '/product/getProductListByCategory', params, 'POST',
     function(data) {
         console.log(data);
-        app.recmdList = [];
-        var result = data.result
-        for(var i = 0; result.length; i++) {
-            if(i > 9) {
-                break;
-            }
-            app.recmdList.push(result[i]);
-        }
+        app.recmdList = data.result;
+        
+        insertRecommandListHtml();
 
     }, function(err) {
         console.error(err);
