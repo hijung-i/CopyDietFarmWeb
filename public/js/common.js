4acf3a0a1bf635502ba3a91eed15dbbbd45ca639
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-06-18 15:59:48 +0900

    운영 url 전환

--- public/js/common.js
+++ public/js/common.js
@@ -1,16 +1,19 @@
 // var API_SERVER = "http://localhost:9090";
-var SERVER_IP = 'localhost';
+// var SERVER_IP = 'localhost';
 // var SERVER_IP = '192.168.0.3';
 // var SERVER_IP = 'data-flow.co.kr';
-// var SERVER_IP = 'dietfarm.co.kr';
 
 // var CALLBACK_SERVER = "http://"+ SERVER_IP +":3000";
-var CALLBACK_SERVER = "http://"+ SERVER_IP;
+
 // var API_SERVER = "http://"+ SERVER_IP +":9090";
 
-var API_SERVER = "http://112.217.209.162:9090";
+// var API_SERVER = "http://112.217.209.162:9090";
 // var RESOURCE_SERVER = "http://112.217.209.162:8000";
-// var API_SERVER = "http://13.209.123.102:9090";
+
+// 운영
+var API_SERVER = "http://13.209.123.102:9090";
+var SERVER_IP = 'dietfarm.co.kr';
+var CALLBACK_SERVER = "http://"+ SERVER_IP;
 
 var RESOURCE_SERVER = "http://13.209.123.102:8000";
 
