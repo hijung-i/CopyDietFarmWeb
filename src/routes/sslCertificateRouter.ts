540f21f50d31ac42c7d684d352d76001f829d0db
Ubuntu <ubuntu@ip-172-30-2-31.ap-northeast-2.compute.internal>
2021-06-19 07:43:56 +0000

    인증서추가

--- src/routes/sslCertificateRouter.ts
+++ src/routes/sslCertificateRouter.ts
@@ -6,14 +6,15 @@ import * as path from 'path'
 const router = Router()
 
 router.get('/acme-challenge/:fileName', async (req: Request, res: Response, next: NextFunction) => {
-    console.log('GET acme-challenge --> ', req.params)
     const acmeDir = '.well-known/acme-challenge'
     const fileName = req.params.fileName
-    const filePath = path.join(acmeDir, fileName)
-    console.log(globalData.getBaseDir(), filePath)
+    const filePath = path.join(globalData.getBaseDir(), path.join(acmeDir, fileName))
+    console.log(filePath)
 
     try {
-        fs.readFile(path.join(globalData.getBaseDir(), filePath), { encoding: 'utf-8' }, (data) => {
+        fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+	    if (err) console.log(err)
+	    console.log(data)
             res.send(data)
         })
     } catch (err) {
