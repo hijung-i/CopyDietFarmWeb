2402379c34f28135b79319693da1ac95b8fba89c
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-06-19 16:08:15 +0900

    challenge

--- src/routes/sslCertificateRouter.ts
+++ src/routes/sslCertificateRouter.ts
@@ -12,17 +12,14 @@ router.get('/acme-challenge/:fileName', async (req: Request, res: Response, next
     const fileName = req.params.fileName
     const filePath = path.join(acmeDir, fileName)
 
-    let file;
-    try{
-        file = fs.readFile(path.join(globalData.getBaseDir(), filePath), { encoding: 'utf-8' }, (data) => {
-            res.send(data);
+    try {
+        fs.readFile(path.join(globalData.getBaseDir(), filePath), { encoding: 'utf-8' }, (data) => {
+            res.send(data)
         })
-
-    } catch(err) {
+    } catch (err) {
         res.send('')
         console.error(err)
     }
-
 })
 
 export default router
