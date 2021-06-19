22d9ba80f9c83eb0773828501680fc9b6ad9f2c2
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-06-19 16:40:13 +0900

    인증서 적용

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,21 @@
-import app from './app'
+import app, { globalData } from './app'
+import * as fs from 'fs'
 
-app.listen(80, () => {
+import * as http from 'http'
+import * as https from 'https'
+
+const options = {
+    pfx: fs.readFileSync(globalData.getBaseDir() + '/cert/cert.pfx'),
+    passphrase: 'dataflow0327!@'
+}
+
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(options, app)
+
+httpServer.listen(80, () => {
     console.log('SERVER IS LISTENING PORT 80')
+})
+
+httpsServer.listen(443, () => {
+    console.log('SERVER IS LISTENING PORT 443')
 })
