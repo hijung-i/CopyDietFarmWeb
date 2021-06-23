779393ea5869f100d8e642ab7727d090ce421114
hKim95S2 <hlkim@data-flow.co.kr>
2021-06-23 13:50:21 +0900

    ㅇㅇ

--- public/js/product.js
+++ public/js/product.js
@@ -402,7 +402,7 @@ function insertRecommandListHtml() {
             break;
         }
         var product = recmdList[i];
-        html += '<div><a href=""><img src="'+ RESOURCE_SERVER + product.url +'" alt="상품 이미지'+ (i+1)+'"></a></div>'
+         html += '<div><a href="/product/' + product.productCode +'"><img src="'+RESOURCE_SERVER + product.url +'" alt="'+(i+1)+'/'+ products.length+'"><div class="desc"><p class="title">' +product.productName + '</p><ul><li class="sale">' + numberFormat(product.discountPrice) + '원</li><li class="cost">' + numberFormat(product.retailPrice) + '원</li><li class="ratio">' + Math.round(product.discountRate, 0) + '%</li></ul></div></a></div>'
     }
 
     $('.responsive1').html(html);
