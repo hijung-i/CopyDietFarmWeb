61be42becc81f8c2e06b03dcea75828d9b6a54d9
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-07-12 21:13:31 +0900

    예외처리 적용

--- src/routes/snsLoginRouter.ts
+++ src/routes/snsLoginRouter.ts
@@ -52,56 +52,67 @@ router.get('/result/kakao', async (req: Request, res: Response, next: NextFuncti
 
 router.get('/result/naver', async (req: Request, res: Response, next: NextFunction) => {
     console.log('GET /result/naver req.query >> ', req.query)
+    try {
+        const callback = {
+            type: 'N',
+            code: req.query.code,
+            tokenNaver: req.query.tokenNaver,
+            userId: req.query.userId,
+            password: req.query.password,
+            userEmail: req.query.userEmail,
+            userGender: req.query.userGender,
+            userCellNo: req.query.userCellNo,
+            userInfo: req.query.userInfo,
+            userName: req.query.userName
+        }
 
-    const callback = {
-        type: 'N',
-        code: req.query.code,
-        tokenNaver: req.query.tokenNaver,
-        userId: req.query.userId,
-        password: req.query.password,
-        userEmail: req.query.userEmail,
-        userGender: req.query.userGender,
-        userCellNo: req.query.userCellNo,
-        userInfo: req.query.userInfo,
-        userName: req.query.userName
+        res.locals.callback = callback
+
+        render(req, res, 'loginCallback', {})
+    } catch (error) {
+        console.log('에러 발생', error)
+        STREAM.writeError(JSON.stringify(error))
+        res.send('<script>alert("오류가 발생했습니다."); location.href="/";</script>')
     }
-
-    res.locals.callback = callback
-
-    render(req, res, 'loginCallback', {})
 })
 
 router.get('/callback/naver', (req: Request, res: Response, next: NextFunction) => {
     console.log('GET /callback/naver req.query >> ', req.query)
 
-    const code = req.query.code
-    const state = req.query.state
-    console.log(code, state)
-    const api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
-    + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state
-    const request = require('request')
-    const options = {
-        url: api_url,
-        headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
-    }
-    request.get(options, async function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            const obj = JSON.parse(body)
-
-            let result = await userService.getNaverUserInfo(obj.access_token)
-            console.log('result >>>>', result)
-            if (result !== null) {
-                res.send('<script>opener.naverCallback(true, \'' + JSON.stringify(result) + '\');window.close();</script>')
-                return
-            }
-
-            res.send('<script>opener.naverCallback(false)</script>')
-        } else {
-            res.send('<script>opener.naverCallback(false)</script>')
-            console.log('error = ' + response.statusCode)
+    try {
+        const code = req.query.code
+        const state = req.query.state
+        console.log(code, state)
+        const api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+        + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state
+        const request = require('request')
+        const options = {
+            url: api_url,
+            headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
         }
-    })
 
+        request.get(options, async function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                const obj = JSON.parse(body)
+
+                let result = await userService.getNaverUserInfo(obj.access_token)
+                console.log('result >>>>', result)
+                if (result !== null) {
+                    res.send('<script>opener.naverCallback(true, \'' + JSON.stringify(result) + '\');window.close();</script>')
+                    return
+                }
+
+                res.send('<script>opener.naverCallback(false)</script>')
+            } else {
+                res.send('<script>opener.naverCallback(false)</script>')
+                console.log('error = ' + response.statusCode)
+            }
+        })
+    } catch (error) {
+        console.log('에러 발생', error)
+        STREAM.writeError(JSON.stringify(error))
+        res.send('<script>alert("오류가 발생했습니다."); location.href="/";</script>')
+    }
 })
 
 router.post('/callback/apple', (req: Request, res: Response, next: NextFunction) => {
