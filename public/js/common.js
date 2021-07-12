f375fe5f3674a16acb60e038b732d2aff783889c
hKim95S2 <hlkim@data-flow.co.kr>
2021-07-12 17:19:50 +0900

    수정

--- public/js/common.js
+++ public/js/common.js
@@ -545,3 +545,16 @@ function scrollAllow() {
    $('html, body').css({'overflow': 'visible'});
    $('html,body').off('scroll touchmove mousewheel');
 }
+
+function clip(){
+
+	var url = '';
+	var textarea = document.createElement("textarea");
+	document.body.appendChild(textarea);
+	url = window.document.location.href;
+	textarea.value = url;
+	textarea.select();
+	document.execCommand("copy");
+	document.body.removeChild(textarea);
+	alert("URL이 복사되었습니다.")
+}
