403887ee16421918133f93f3b1d1c54d1f717e9a
hKim95S2 <hlkim@data-flow.co.kr>
2021-07-23 15:46:49 +0900

    ㅇㅇ

--- src/configs/winston.ts
+++ src/configs/winston.ts
@@ -1,42 +1,42 @@
-// import * as path from 'path'
-// import * as winston from 'winston'
-// 
-// import { globalData } from '../app'
-// const _LOG_DIR = path.join(globalData.getBaseDir(), 'logs')
-// 
-// const infoTransport = new winston.transports.File({
-//     filename: 'debug.log',
-//     dirname: _LOG_DIR,
-//     level: 'debug'
-// })
-// 
-// const errorTransport = new winston.transports.File({
-//     filename: 'error.log',
-//     dirname: _LOG_DIR,
-//     level: 'error'
-// })
-// 
-// const logger = winston.createLogger({
-//     transports: [infoTransport, errorTransport]
-// })
-// 
-// const stream = {
-//     writeDebug: (message: any) => {
-//         try {
-//             logger.debug(new Date().toLocaleString() + ': ' + message)
-//         } catch (err) {
-//             logger.error('failed to log', JSON.stringify(err))
-//         }
-//     },
-//     writeError: (message: any) => {
-//         try {
-//             logger.error(new Date().toLocaleString() + ': ' + message)
-//         } catch (err) {
-//             logger.error('failed to log', JSON.stringify(err))
-//         }
-//     }
-// }
-// 
-// export {
-//     logger, stream
-// }
+import * as path from 'path'
+import * as winston from 'winston'
+
+import { globalData } from '../app'
+const _LOG_DIR = path.join(globalData.getBaseDir(), 'logs')
+
+const infoTransport = new winston.transports.File({
+    filename: 'debug.log',
+    dirname: _LOG_DIR,
+    level: 'debug'
+})
+
+const errorTransport = new winston.transports.File({
+    filename: 'error.log',
+    dirname: _LOG_DIR,
+    level: 'error'
+})
+
+const logger = winston.createLogger({
+    transports: [infoTransport, errorTransport]
+})
+
+const stream = {
+    writeDebug: (message: any) => {
+        try {
+            logger.debug(new Date().toLocaleString() + ': ' + message)
+        } catch (err) {
+            logger.error('failed to log', JSON.stringify(err))
+        }
+    },
+    writeError: (message: any) => {
+        try {
+            logger.error(new Date().toLocaleString() + ': ' + message)
+        } catch (err) {
+            logger.error('failed to log', JSON.stringify(err))
+        }
+    }
+}
+
+export {
+    logger, stream
+}
