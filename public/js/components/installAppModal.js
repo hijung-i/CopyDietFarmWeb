73a3d843e00df6031590467c50924b7293d9d2bd
hKim95S2 <hlkim@data-flow.co.kr>
2021-08-04 09:30:30 +0900

    모달수정

--- public/js/components/installAppModal.js
+++ public/js/components/installAppModal.js
@@ -2,15 +2,18 @@ var installAppTemplate = '';
 
 installAppTemplate += '<div class="popup_box">'
 installAppTemplate += '<div class="popup_cont">'
-installAppTemplate += '<div class="index-modal">'
-installAppTemplate += '<p class="sale_coupon"><img src="/images/coupon_icon_friend@2x.png"></p>' 
-installAppTemplate += '<span class="close" @click="closePopupModal();">x</span>' 
-installAppTemplate += '<p class="app01">앱 설치하러가기</p>' 
-installAppTemplate += '<p @click="closePopupModal();"><span>아니요. 그냥 웹으로 볼래요.</span></p>' 
-installAppTemplate += '<a href="https://dietfarm.page.link/pzok"><p class="app02">앱 설치하기> </p></a>'
-installAppTemplate += '</div>' 
+installAppTemplate += '    <div class="index-modal">'
+installAppTemplate += '        <p class="sale_coupon"><img src="/images/diet_farm_logo.png" style="width:140px;height:80px"></p>'
+installAppTemplate += '        <span class="close" @click="closePopupModal();">x</span>'
+installAppTemplate += '        <p class="app01">앱 설치하러가기</p>'
+installAppTemplate += '        <a href="https://dietfarm.page.link/pzok">'
+installAppTemplate += '            <p class="app02">앱 설치하기</p>'
+installAppTemplate += '        </a>'
+installAppTemplate += '        <p @click="todaycloseWin();"><span>아니요. 그냥 웹으로 볼래요.</span></p>'
+installAppTemplate += '    </div>'
 installAppTemplate += '</div>'
 installAppTemplate += '</div>'
+
 
 var installAppModal = {
     template: installAppTemplate,
@@ -24,3 +27,31 @@ var installAppModal = {
  
     }
 }
+
+  $( document ).ready(function() {
+        cookiedata = document.cookie;
+        console.log(cookiedata);
+        if ( cookiedata.indexOf("mcookie=done") < 0 ){
+            console.log('cookie 없음')
+            document.getElementById('popup_box').style.display = "block";
+        } else {
+            console.log("cookie 있음")
+            document.getElementById('popup_box').style.display = "none";
+        }
+    });
+    function setCookie( name, value, expiredays ) { 
+        var todayDate = new Date();
+        todayDate.setDate( todayDate.getDate() + expiredays );
+        document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+    }
+    
+    function closeWin() {
+            document.getElementById('popup_box').style.display = "none";
+    
+    }
+    
+    function todaycloseWin() {
+        setCookie("mcookie", "done", 1);
+        document.getElementById('popup_box').style.display = "none";
+    
+    }
