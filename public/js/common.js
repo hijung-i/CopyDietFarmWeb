c0433e96daeaf7ecca113c5585dd880226a90d00
hKim95S2 <hlkim@data-flow.co.kr>
2021-08-13 14:10:51 +0900

    수정 배포

--- public/js/common.js
+++ public/js/common.js
@@ -591,3 +591,32 @@ function masking(str) {
    }
    return temp
 }
+
+
+
+var keydownCtrl = 0;
+var keydownShit = 0;
+
+document.onkeydown=keycheck;
+document.onkeyup=uncheckCtrlShift;
+
+function keycheck()
+{
+   switch(event.keyCode){
+      case 123:event.keyCode='';return false; break; //F12
+      case 17:event.keyCode='';keydownCtrl=1;return false;break;//컨트롤키
+   }
+   if(keydownCtrl) return false;
+}
+
+function uncheckCtrlShift(){
+   if(event.keycode==17) keydownCtrl=0;
+   if(event.keyCode==16) keydownShift=0;
+}
+
+function click(){
+   if ((event.button==2) || (event.button==2)){
+      alert('클릭할 수 없습니다.')
+   }
+}
+document.onmousedown=click;
