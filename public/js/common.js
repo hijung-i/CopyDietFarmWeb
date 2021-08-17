ec0496a37d0666c193da582de8528eed7ddc693d
jgpark-dataflow <jgpark@data-flow.co.kr>
2021-08-17 15:00:59 +0900

    f12, copy block

--- public/js/common.js
+++ public/js/common.js
@@ -594,25 +594,25 @@ function masking(str) {
 
 
 //무단복사, f12키 금지 코드(수정할땐 주석처리)
-// var keydownCtrl = 0;
-// var keydownShit = 0;
+var keydownCtrl = 0;
+var keydownShit = 0;
 
-// document.onkeydown=keycheck;
-// document.onkeyup=uncheckCtrlShift;
+document.onkeydown=keycheck;
+document.onkeyup=uncheckCtrlShift;
 
-// function keycheck()
-// {
-//    switch(event.keyCode){
-//       case 123:event.keyCode='';return false; break; //F12
-//       case 17:event.keyCode='';keydownCtrl=1;return false;break;//컨트롤키
-//    }
-//    if(keydownCtrl) return false;
-// }
+function keycheck()
+{
+   switch(event.keyCode){
+      case 123:event.keyCode='';return false; break; //F12
+      case 17:event.keyCode='';keydownCtrl=1;return false;break;//컨트롤키
+   }
+   if(keydownCtrl) return false;
+}
 
-// function uncheckCtrlShift(){
-//    if(event.keycode==17) keydownCtrl=0;
-//    if(event.keyCode==16) keydownShift=0;
-// }
+function uncheckCtrlShift(){
+   if(event.keycode==17) keydownCtrl=0;
+   if(event.keyCode==16) keydownShift=0;
+}
 
 function click(){
    if ((event.button==2) || (event.button==2)){
